b343153d7219071b1eafd7d35ed3b6ff7252b251
Hawn09 <91580993+Hawn09@users.noreply.github.com>
2022-11-20 23:44:56 +0900

    11.20

--- game/Assets/Scripts/Transport.cs
+++ game/Assets/Scripts/Transport.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Transport : MonoBehaviour
@@ -7,7 +8,17 @@ public class Transport : MonoBehaviour
     public float Speed = 0.01f;
     float forward = 1f;
     SpriteRenderer rend;
-    // Start is called before the first frame update
+
+    private void Awake()
+    {
+        GameObject[] go = GameObject.FindGameObjectsWithTag("MinePlane");
+
+        if (go != null)
+        {
+            Debug.Log("Ã£À½");
+        }
+    }
+
     void Start()
     {
         rend = GetComponent<SpriteRenderer>();
@@ -17,7 +28,7 @@ public class Transport : MonoBehaviour
     void FixedUpdate()
     {
         transform.position += new Vector3(forward * Speed, 0, 0);
-        if (transform.position.x > 9)
+        if (transform.position.x > 9 )
         {
             forward = -1;
             rend.flipX = true;
@@ -27,8 +38,25 @@ public class Transport : MonoBehaviour
             forward = 1;
             rend.flipX = false;
         }
-
-        
-
     }
+
+    void goAwake()
+    {
+        Invoke("Awake", 2);
+    }
+  
+
+    //void getMove()
+    //{
+    //    if (transform.position.x > go.transform.position.x)
+    //    {
+    //        forward = -1;
+    //        rend.flipX = true;
+    //    }
+    //    else if (transform.position.x < go.transform.position.x)
+    //    {
+    //        forward = 1;
+    //    }
+    //    else (transform.position.x == go.transform.position.x)forward = 0;
+    //}
 }
