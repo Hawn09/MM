8e8c7fcc1b024b22938de052285120d4b7115c8e
kohaerang <kohaerang@gmail.com>
2022-11-28 19:24:39 +0900

    1128
    펭귄해결

--- game/Assets/Scripts/SenSor.cs
+++ game/Assets/Scripts/SenSor.cs
@@ -6,18 +6,34 @@ using UnityEngine;
 
 public class SenSor : MonoBehaviour
 {
+<<<<<<< HEAD
+    public bool find = false;
+    public float Speed = 0.01f;
+    float forward = 1f;
+    float time = 0.0f;
+    public Transform parent;
+=======
     
+>>>>>>> d4a75677150a1011756151ed2bef498b6ab1e8a9
+
     public bool detect = false;
     // Start is called before the first frame update
     void Start()
     {
-
+        
     }
 
     // Update is called once per frame
     void Update()
-    { 
-    
+    {
+        time += Time.deltaTime;
+
+        if (time >= 10)
+        {
+            Destroy(gameObject);
+
+            Debug.Log("Destroyed");
+        }
     }
     void OnTriggerEnter(Collider col)
     {
@@ -25,13 +41,37 @@ public class SenSor : MonoBehaviour
         {
             detect = true;
             Debug.Log("detect =" + detect);
+<<<<<<< HEAD
+            findpeng = Instantiate(prefab, parent);
+
+            
+
+
+=======
+            
+>>>>>>> d4a75677150a1011756151ed2bef498b6ab1e8a9
         }
         
     }
+    public GameObject prefab;
+    GameObject findpeng;
     void FixedUpdate()
     {
-        
-        
+        if (find == false)
+        {
+            transform.position += new Vector3(forward * Speed, 0, 0);
+            if (transform.position.x > 9)
+            {
+                forward = -1;
+                
+            }
+            else if (transform.position.x < -9)
+            {
+                forward = 1;
+                
+            }
+        }
+
     }
 }
 
